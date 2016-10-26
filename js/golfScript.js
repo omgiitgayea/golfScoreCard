4734afc7a0392ee012651d9a12505734f573810b
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-26 08:40:13 -0600

    sliding experiment

--- js/golfScript.js
+++ js/golfScript.js
@@ -1,3 +1,28 @@
 /**
  * Created by GodaiYuusaku on 10/26/16.
  */
+$("#pro").click(function () {
+    $("#champion").slideUp();
+    $("#men").slideUp();
+    $("#women").slideUp();
+});
+
+$("#champion").click(function () {
+    $("#pro").slideUp();
+    $("#men").slideUp();
+    $("#women").slideUp();
+});
+
+$("#pro").click(function () {
+    $("#champion").slideUp();
+    $("#champion").css("display", "none");
+    $("#men").slideUp();
+    $("#women").slideUp();
+});
+
+$("#pro").click(function () {
+    $("#champion").slideUp();
+    $("#champion").css("display", "none");
+    $("#men").slideUp();
+    $("#women").slideUp();
+});
