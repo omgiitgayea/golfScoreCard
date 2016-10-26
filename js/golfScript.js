9d27ad6b1baef8364d0a98dd74071c4284ad0449
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-26 10:02:07 -0600

    sliding works!

--- js/golfScript.js
+++ js/golfScript.js
@@ -1,28 +1,8 @@
 /**
  * Created by GodaiYuusaku on 10/26/16.
  */
-$("#pro").click(function () {
-    $("#champion").slideUp();
-    $("#men").slideUp();
-    $("#women").slideUp();
-});
 
-$("#champion").click(function () {
-    $("#pro").slideUp();
-    $("#men").slideUp();
-    $("#women").slideUp();
-});
-
-$("#pro").click(function () {
-    $("#champion").slideUp();
-    $("#champion").css("display", "none");
-    $("#men").slideUp();
-    $("#women").slideUp();
-});
-
-$("#pro").click(function () {
-    $("#champion").slideUp();
-    $("#champion").css("display", "none");
-    $("#men").slideUp();
-    $("#women").slideUp();
-});
+$(".tees").click(function () {
+    $(".tees").slideToggle();
+    $(this).stop();
+})
