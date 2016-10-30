f5ceca90df3b806fb3e6c7b60d15112be10cb5bf
Christopher Rescorla <cgrescorla@gmail.com>
2016-10-30 16:50:30 -0600

    many error checks, added in values for tee yardages and started code to make par reflect tee selected

--- js/golfScript.js
+++ js/golfScript.js
@@ -4,30 +4,58 @@
 var TEES_ALPHA = 0.8;           // the alpha for the tee rows
 var MAX_PLAYERS = 10;
 var courseData = null;
-var numPlayers = 0;
 var players = [];
 var addedPlayers = 0;
+var roundBegun = false;
+var currentTee = "";
 
 function initCard() {
     // place the holes header on the screen
-    var roundBegun = false;
-    var holeHeader = "<header id='holeHeader'>HOLES</header>";
-    var currentTee = "";
+    var holeHeader = "<header id='holeHeader'><div class='headers'>HOLES</div></header>";
     var courseHoles = courseData.course.holes;
 
     $("#teesContainer").append(holeHeader);
+    for (var i = 0; i < courseHoles.length; i++) {
+        var holeNumDiv = "<div id='holeNo" + i + "' class='yardage'>" + (i + 1) + "</div>";
+        $("#holeHeader").append(holeNumDiv);
+        if (i % 9 === 8) {
+            var holeLabel = "<div id='holeBreak" + Math.floor(i / 9) + "' class='yardage'></div>";
+            $("#holeHeader").append(holeLabel);
+            if (i === 8) {
+                $("#holeBreak0").html("OUT");
+            }
+            else {
+                $("#holeBreak1").html("IN");
+            }
+        }
+    }
+    var totalHeader = "<div id='total' class='yardage'>TOTAL</div>";
+    $("#holeHeader").append(totalHeader);
 
     // get the tee type, tee color and add them to the screen (as it is now, I may have to put far too much code in this getJSON function)
     for (var i = 0; i < courseData.course.tee_types.length; i++) {
         var teeID = courseData.course.tee_types[i].tee_type;              // get the name of the tee type
-        var tee = "<div id='" + teeID + "' class='tees'></div>";     // creates a div with the id teeID and class tees
+        var tee = "<div id='" + teeID + "' class='tees'><div class='headers'>" + teeID.toUpperCase() + "</div></div>";     // creates a div with the id teeID and class tees
         $("#teesContainer").append(tee);
-        $("#" + teeID).html(teeID.toUpperCase());
         $("#" + teeID).css("background-color", hexToRgba(courseData.course.tee_types[i].tee_hex_color));
-    }
+        for (var j = 0; j < courseHoles.length; j++) {
+            var teeYardsDiv = "<div id='" + teeID + j + "' class='yardage'>" + courseData.course.holes[j].tee_boxes[i].yards + "</div>";
+            $("#" + teeID).append(teeYardsDiv);
+            if (j % 9 === 8) {
+                var totalYrdsDiv = "<div id='" + teeID + "Total" + (j + 1) + "' class='yardage'>";
+                if (j === 8) {
+                    totalYrdsDiv += courseData.course.tee_types[i].front_nine_yards + "</div>";
+                }
+                else if (j === 17) {
+                    totalYrdsDiv += courseData.course.tee_types[i].back_nine_yards + "</div>";
+                }
+                $("#" + teeID).append(totalYrdsDiv);
+            }
+        }
+        var courseYrdsDiv = "<div id='" + teeID + "TotalYrd' class='yardage'>" + courseData.course.tee_types[i].yards + "</div>";
+        $("#" + teeID).append(courseYrdsDiv);
 
-    var parContainer = "<div id='parValues'>PAR</div>";
-    $("#teesContainer").append(parContainer);
+    }
 
     $("#mainContainer").append("<div id='startRound'><button id='roundStart'>Begin Round</button><div id='missingTeeType'></div></div>");
 
@@ -38,9 +66,31 @@ function initCard() {
             $(this).stop();
             if (currentTee === "") {
                 currentTee = $(this).attr("id");
+
+                //add in par code
+                var parContainer = "<div id='parValues'><div class='headers'>PAR</div></div>";
+                $("#teesContainer").append(parContainer);
+                for (var i = 0; i < courseHoles.length; i++) {
+                    var parDiv = "<div id='par" + i + "' class='yardage'>" + 2000 + "</div>";
+                    $("#parValues").append(parDiv);
+                    if (i % 9 === 8) {
+                        var parBreakDIV = "<div id='parBreak" + Math.floor(i / 9) + "' class='yardage'>18000</div>";
+                        $("#parValues").append(parBreakDIV);
+                        if (i === 8) {
+                            $("#parBreak").html("OUT");
+                        }
+                        else {
+                            $("#parBreak").html("IN");
+                        }
+                    }
+                }
+                var totalParHeader = "<div id='totalPar' class='yardage'>36000</div>";
+                $("#parValues").append(totalParHeader);
+
             }
             else {
                 currentTee = "";
+                $("#parValues").remove();
             }
         }
     });
@@ -62,51 +112,47 @@ function initCard() {
 $("#courseSelectBtn").click(function () {
     $("#loading").show();
     // setTimeout(function () {            // setTimeout is only there for testing of loading animation
-        $.getJSON("https://golf-courses-api.herokuapp.com/courses/11819", function (data) {
-            courseData = data;
-            $("#courseSelectBtn").remove();
-            $("#loading").remove();
+    $.getJSON("https://golf-courses-api.herokuapp.com/courses/11819", function (data) {
+        courseData = data;
+        $("#courseSelectBtn").remove();
+        $("#loading").remove();
 
-            var newPlayer = "<div id='newPlayerDiv'><input type='number' id='playerNumbers' min='1' max='10' value='1'><button id='newPlayerBtn'>Add player(s)</button><span id='invalidPlayersMsg'></span></div>";
-            var scoreContainer = "<div id='scoreCard'></div>";
-            var courseName = "<div id='courseContainer'>" + courseData.course.name + "</div>";
-            var cardContainer = "<div id='teesContainer'></div>";
-            $("#mainContainer").append(courseName, cardContainer, scoreContainer);
-            $("#scoreCard").append(newPlayer);
+        var newPlayer = "<div id='newPlayerDiv'><input type='number' id='playerNumbers' min='1' max='" + MAX_PLAYERS + "' value='1'><button id='newPlayerBtn'>Add player(s)</button><span id='invalidPlayersMsg'></span></div>";
+        var scoreContainer = "<div id='scoreCard'></div>";
+        var courseName = "<div id='courseContainer'>" + courseData.course.name + "</div>";
+        var cardContainer = "<div id='teesContainer'></div>";
+        $("#mainContainer").append(courseName, cardContainer, scoreContainer);
+        $("#scoreCard").append(newPlayer);
 
-            $("#newPlayerBtn").click(function () {
-                var addnPlayers = $("#playerNumbers").val();
+        $("#newPlayerBtn").click(function () {
+            var addnPlayers = $("#playerNumbers").val();
 
-                if (addnPlayers > 0 && (Number(addnPlayers) + players.length) <= 10)
-                {
-                    $("#invalidPlayersMsg").html("");
-                    if (players.length <= 0) {
-                        $("#teesContainer").css("display", "inline-block");
-                        initCard();
-                    }
-                    addPlayer(Number(addnPlayers));
+            if (addnPlayers > 0 && (Number(addnPlayers) + players.length) <= MAX_PLAYERS) {
+                $("#invalidPlayersMsg").html("");
+                if (players.length <= 0) {
+                    $("#teesContainer").css("display", "inline-block");
+                    initCard();
                 }
-                else if (addnPlayers <= 0)
-                {
-                    $("#invalidPlayersMsg").html("At least one player must play...");
-                }
-                else if ((Number(addnPlayers) + players.length) > 10)
-                {
-                    $("#invalidPlayersMsg").html("Only 10 players can play at a time");
-                }
+                addPlayer(Number(addnPlayers));
+            }
+            else if (addnPlayers <= 0) {
+                $("#invalidPlayersMsg").html("At least one player must play...");
+            }
+            else if ((Number(addnPlayers) + players.length) > MAX_PLAYERS) {
+                $("#invalidPlayersMsg").html("Only " + MAX_PLAYERS + " players can play at a time");
+            }
 
-                $("#playerNumbers").val("1");
+            $("#playerNumbers").val("1");
 
-
-            });
 
         });
+
+    });
     // }, 1000);
 
 });
 
-function addPlayer(morePlayers)
-{
+function addPlayer(morePlayers) {
     $("#playerModal").css("display", "block");
     $("#modalContent").html("Enter Player Names:");
     for (var i = 0; i < morePlayers; i++) {
@@ -116,8 +162,7 @@ function addPlayer(morePlayers)
     $("#modalContent").slideDown();
 
     var btnText = "Add Player";
-    if (morePlayers > 1)
-    {
+    if (morePlayers > 1) {
         btnText += "s"
     }
     $("#playerSubmitBtn").html(btnText);
@@ -127,15 +172,13 @@ function addPlayer(morePlayers)
         var compInputs = [];
         for (var i = 0; i < morePlayers; i++) {
             var playerInput = $("#newPlayer" + i);
-            if (playerInput.val() === null || $.trim(playerInput.val()) === "" || (players.indexOf(playerInput.val()) != -1 && players.length > 0))
-            {
+            if (playerInput.val() === null || $.trim(playerInput.val()) === "" || (players.indexOf(playerInput.val()) != -1 && players.length > 0)) {
                 valid = false;
                 playerInput.val("");
                 playerInput.css("border-color", "red");
                 playerInput.css("background-color", "pink");
             }
-            if (compInputs.indexOf(playerInput.val()) != -1 && compInputs.length > 0)
-            {
+            if (compInputs.indexOf(playerInput.val()) != -1 && compInputs.length > 0) {
                 valid = false;
                 playerInput.val("");
                 playerInput.css("border-color", "red");
@@ -145,14 +188,23 @@ function addPlayer(morePlayers)
                 compInputs.push(playerInput.val());
             }
         }
-        compInputs = [];
 
         if (valid) {
             $("#playerModal").css("display", "none");
             for (var i = 0; i < morePlayers; i++) {
                 players.push($("#newPlayer" + i).val());
-                var playerField = "<div id='player" + (i + addedPlayers) + "'>" + $('#newPlayer' + i).val() + "<span class='removePlayerBtn'><i class='fa fa-minus-circle'></i></span></div>";
+                var playerField = "<div id='player" + (i + addedPlayers) + "' class='playerBar'><div class='headers'>" + $('#newPlayer' + i).val() + "<span class='removePlayerBtn'><i class='fa fa-minus-circle'></i></span></div></div>";
                 $("#scoreCard").append(playerField);
+                for (var j = 0; j < courseData.course.holes.length; j++) {
+                    var scoreDiv = "<div class='holeScore'><input id='player" + (i + addedPlayers) + "-" + j + "'type='text' class='scoreField'></div>";
+                    $("#player" + (i + addedPlayers)).append(scoreDiv);
+                    if (j % 9 === 8) {
+                        var scoreTotalDiv = "<div id='player" + (i + addedPlayers) + "Total" + (j + 1) + "' class='yardage'>h</div>";
+                        $("#player" + (i + addedPlayers)).append(scoreTotalDiv);
+                    }
+                }
+                var scoreFinalDiv = "<div id='player" + (i + addedPlayers) + "TotalYrd' class='yardage'>h</div>";
+                $("#player" + (i + addedPlayers)).append(scoreFinalDiv);
             }
             addedPlayers += morePlayers;
         }
@@ -160,18 +212,79 @@ function addPlayer(morePlayers)
         $(".removePlayerBtn").unbind().click(function () {
             var gonePlayerName = $(this).parent().text();
             players.splice(players.indexOf(gonePlayerName), 1);
-            $(this).parent().remove();
-            if (players.length === 0)
-            {
+            $(this).parent().parent().remove();
+            if (players.length === 0) {
                 $("#teesContainer").html("");
                 $("#startRound").remove();
             }
         });
+
+        $(".scoreField").change(function () {
+            if (roundBegun) {
+                $(this).removeClass("invalidScore");
+                var validScore = true;
+                var numOnly = /^[0-9]*$/;
+                if (numOnly.test($(this).val())) {
+                    if ($(this).val() < 1) {
+                        validScore = false;
+                    }
+                }
+                else {
+                    validScore = false;
+                }
+
+                if (!validScore) {
+                    $(this).addClass("invalidScore");
+                    $(this).val("");
+                }
+                else {
+                    var currentPlayer = $(this).attr("id");
+                    var dashLocation = currentPlayer.indexOf("-");
+                    var playerID = currentPlayer.slice(6, dashLocation);
+                    var currentHole = Number(currentPlayer.slice(dashLocation + 1));
+                    var validField = true;
+
+                    if (currentHole != 0) {
+                        for (var testPrev = 0; testPrev < currentHole; testPrev++) {
+                            if ($("#player" + playerID + "-" + testPrev).val() === null || $.trim($("#player" + playerID + "-" + testPrev).val()) === "") {
+                                validField = false;
+                                $("#player" + playerID + "-" + testPrev).addClass("invalidScore");
+                            }
+                        }
+                    }
+                    if (validField) {
+                        $(".scoreField").removeClass("invalidScore");
+                        var activeScoreDivID = "";
+                        var roundFinished = false;
+                        if (currentHole < 9) {
+                            activeScoreDivID = "player" + playerID + "Total" + 9;
+                        }
+                        else {
+                            activeScoreDivID = "player" + playerID + "Total" + 18;
+                        }
+                        if (currentHole === (courseData.course.holes.length - 1)) {
+                            roundFinished = true;
+                        }
+                    }
+                    else {
+                        $(this).val("");
+                    }
+
+
+                    // rejigger for lets say readonly status to change for just the ones in that row, not sure how yet...or make it readonly when the final player does it, that's easier
+                    // if (roundFinished) {
+                    //     $(".scoreField").prop("readonly", true);
+                    //     $(".scoreField").css("border", "none");
+                    //     console.log("Yay, no more golf!");
+                    // }
+                }
+            }
+            else {
+                $(this).val("");
+            }
+        })
     });
 }
-
-
-
 
 
 // for changing hex color to rgba color
