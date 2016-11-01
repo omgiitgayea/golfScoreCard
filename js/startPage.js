47f2e3265c03230e521e090427b061b116e6d812
Christopher Rescorla <cgrescorla@gmail.com>
2016-11-01 17:14:52 -0600

    built menu with closest courses, work gets in the way of getting more on it

--- js/startPage.js
+++ js/startPage.js
@@ -2,10 +2,9 @@
  * Created by GodaiYuusaku on 11/1/16.
  */
 // this is where we build up the course selection page
-var RADIUS = 15;
+var RADIUS = 50 / 0.621371;         // that's 50 miles converted to km because you know, America
 
-$(document).ready(function()
-{
+$(document).ready(function () {
     getGeolocation();
 
     // function getCourses(lat, lng, rad) {
@@ -22,17 +21,27 @@ $(document).ready(function()
 });
 
 
-function getGeolocation()
-{
-    return new Promise(function(resolve, reject)
-    {
+function getGeolocation() {
+    return new Promise(function (resolve, reject) {
         var options = {
             enableHighAccuracy: true
         };
 
         function success(pos) {
-            getCourses(pos.coords).then(function(courses) {
-                //what to do with the data
+            getCourses(pos.coords).then(function (courses) {
+                    //what to do with the data
+                    console.log(courses.length);
+                    for (var i = 0; i < courses.length; i++) {
+                        var courseName = courses[i].name;
+                        var city = courses[i].city;
+                        var optionDiv = "<option value='" + courses[i].id + "'>" + courseName + ", " + city + "</option>";
+                        $("#courseSelectMenu").append(optionDiv);
+                        // console.log(courses[i].addr_1);
+                        // console.log(courses[i].state_or_province);
+                        // console.log(courses[i].phone);
+                        // console.log(courses[i].zip_code);
+                        // console.log(courses[i].country);
+                    }
                 }
             );
             resolve(pos);
@@ -46,48 +55,16 @@ function getGeolocation()
     });
 }
 
-function getCourses(positionObj)
-{
+function getCourses(positionObj) {
     var courses;
-    return new Promise(function(resolve, reject) {
-        console.log(positionObj.latitude);
-        $.post("http://golf-courses-api.herokuapp.com/courses"), {
+    return new Promise(function (resolve, reject) {
+        $.post("http://golf-courses-api.herokuapp.com/courses", {
             latitude: positionObj.latitude,
             longitude: positionObj.longitude,
             radius: RADIUS
         }, function (data) {
-            resolve(JSON.parseJSON(data).courses)
-        };
+            resolve(JSON.parse(data).courses)
+        });
     })
 }
-
-// $.getJSON("https://golf-courses-api.herokuapp.com/courses/11819", function (coursesData) {
-//     console.log("called");
-//     var courseName = coursesData.course.name;
-//     var optionDiv = "<option value='11819'>"+ courseName + "</option>";
-//     $("#courseSelectMenu").append(optionDiv);
-// });
-
-
-
-// need promise for post for getting the data
-// need promise for geolocation
-
-// function asyncZatannaSpell(stringArray)
-// {
-//     promise = new Promise(execute);
-//     return promise;
-//
-//     function execute(resolve, reject) {
-//         setTimeout(function ()
-//         {
-//             if (stringArray.length <= 0)
-//             {
-//                 reject("How can she say a spell with no words!?")
-//             }
-//             zatanna(stringArray);
-//             resolve();
-//         }, 3000);
-//     }
-// }
 
